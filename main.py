4efddb5e5a5d7cf81be7d741b725a93fa5198ba5
Kennyg1999 <67302875+Kennyg1999@users.noreply.github.com>
2020-09-01 01:46:11 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Exercise 02: Programming with Github
+
+# Name: Kenneth Galvan
+# CIT 127 CRN: 75630 
+# Semester: Fall 2020
+
+# Step 3: Input Taken below
+myInput = input("")
+
+# Step 4: Output message "Hello" with your Name
+print("Hello, "+myInput)
+
+# Step 5: Output a 15 word or more sentence explaining what challenges you expect when using Git in software development.
+print("Challenges I expect and have dealt with in software development are configuring software and debugging errors.")
